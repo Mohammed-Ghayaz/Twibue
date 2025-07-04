21f4286885f30fa9a91d3c6c0a8e15116db54d8f
Mohammed-Ghayaz <mghayaz04@gmail.com>
2025-07-04 16:55:18 +0530

    Add dashboard routes and controller for channel statistics and videos

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import videoRouter from "./routes/video.routes.js"
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 
 app.use("/api/v1/user", userRouter);
@@ -27,5 +28,6 @@ app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/dashboard", dashboardRouter)
 
 export { app };
