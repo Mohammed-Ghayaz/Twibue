a306a3c5c9ac825ff4d61dc4edac80087f4cd5db
Mohammed-Ghayaz <mghayaz04@gmail.com>
2025-07-08 16:38:35 +0530

    Dashboard controller updated

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,6 +8,80 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const totalVideos = await Video.countDocuments({owner: req.user._id})
+
+    const totalSubscribers = await Subscription.countDocuments({channel: req.user._id})
+
+    const result = await Video.aggregate([
+        { $match: { owner: req.user._id } },
+        { $group: {
+            _id: null,
+            totalViews: { $sum: "$views" }
+        }}
+    ]);
+
+    const totalViews = result[0]?.totalViews || 0
+
+    const videoLikesResult = await Like.aggregate([
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "videos"
+            }
+        },
+        {
+            $unwind: "$videos"
+        },
+        {
+            $match: {"videos.owner": req.user._id}
+        },
+        {
+            $group: {
+                _id: null,
+                totalVideoLikes: {$sum: 1}
+            }
+        }
+    ])
+
+    const totalVideoLikes = videoLikesResult[0]?.totalVideoLikes || 0
+
+    const tweetLikesResult = await Like.aggregate([
+        {
+            $lookup: {
+                from: "tweets",
+                localField: "tweet",
+                foreignField: "_id",
+                as: "tweets"
+            }
+        },
+        {
+            $unwind: "$tweets"
+        },
+        {
+            $match: {"tweets.owner": req.user._id}
+        },
+        {
+            $group: {
+                _id: null,
+                totalTweetLikes: {$sum: 1}
+            }
+        }
+    ])
+
+    const totalTweetLikes = tweetLikesResult[0]?.totalTweetLikes || 0
+
+    const stats = {
+        totalVideos,
+        totalViews,
+        totalSubscribers,
+        totalVideoLikes,
+        totalTweetLikes,
+        totalLikes: totalTweetLikes + totalVideoLikes
+    }
+
+    return res.status(200).json(new ApiResponse(200, stats, "Channel Stats retrieved successfully"))
 
 })
 
